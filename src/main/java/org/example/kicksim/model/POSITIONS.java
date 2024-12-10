b180c3e6cd29254cf4d552f5cfe461151787521b
Cucu Raul - Mihai <raul.cucu02@e-uvt.ro>
2024-12-11 01:07:56 +0200

    Inital Player Modal

---
+++ src/main/java/org/example/kicksim/model/POSITIONS.java
@@ -0,0 +1,15 @@
+package org.example.kicksim.model;
+
+public enum POSITIONS {
+    STRIKER,
+    RIGHT_WING,
+    LEFT_WING,
+    CENTRAL_ATTACKING_MIDFIELDER,
+    CENTRAL_MIDFIELDER,
+    RIGHT_MIDFIELDER,
+    LEFT_MIDFIELDER,
+    LEFT_BACK,
+    RIGHT_BACK,
+    CENTER_BACK,
+    GOALKEEPER
+}
--- src/main/java/org/example/kicksim/model/Player.java
+++ src/main/java/org/example/kicksim/model/Player.java
@@ -11,4 +11,17 @@ public class Player {
     private Double skillLevel;
     private Double weight;
     private Double height;
+    private POSITIONS position;
+    private String nationality;
+
+    public Player(String firstName, String lastName, Integer age, Double skillLevel, Double weight, Double height, POSITIONS position, String nationality) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.skillLevel = skillLevel;
+        this.weight = weight;
+        this.height = height;
+        this.position = position;
+        this.nationality = nationality;
+    }
 }
