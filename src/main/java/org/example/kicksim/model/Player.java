d7a0de8b540c357220711673a3a5cac892675cfd
Cucu Raul - Mihai <raul.cucu02@e-uvt.ro>
2024-12-11 01:43:51 +0200

    Added Initial Team Model

--- src/main/java/org/example/kicksim/model/Player.java
+++ src/main/java/org/example/kicksim/model/Player.java
@@ -12,9 +12,9 @@ public class Player {
     private Double weight;
     private Double height;
     private Positions position;
-    private String nationality;
+    private Countries nationality;
 
-    public Player(String firstName, String lastName, Integer age, Double skillLevel, Double weight, Double height, Positions position, String nationality) {
+    public Player(String firstName, String lastName, Integer age, Double skillLevel, Double weight, Double height, Positions position, Countries nationality) {
         this.firstName = firstName;
         this.lastName = lastName;
         this.age = age;
@@ -23,5 +23,69 @@ public class Player {
         this.height = height;
         this.position = position;
         this.nationality = nationality;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+    public Double getSkillLevel() {
+        return skillLevel;
+    }
+
+    public void setSkillLevel(Double skillLevel) {
+        this.skillLevel = skillLevel;
+    }
+
+    public Double getHeight() {
+        return height;
+    }
+
+    public void setHeight(Double height) {
+        this.height = height;
+    }
+
+    public Double getWeight() {
+        return weight;
+    }
+
+    public void setWeight(Double weight) {
+        this.weight = weight;
+    }
+
+    public Countries getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(Countries nationality) {
+        this.nationality = nationality;
+    }
+
+    public Positions getPosition() {
+        return position;
+    }
+
+    public void setPosition(Positions position) {
+        this.position = position;
     }
 }
---
+++ src/main/java/org/example/kicksim/model/Team.java
@@ -0,0 +1,11 @@
+package org.example.kicksim.model;
+
+import java.util.List;
+
+public class Team {
+
+    private String teamName;
+    private Countries teamNationality;
+    private Integer yearFounded;
+    private List<Player> teamPlayers;
+}
