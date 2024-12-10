a2defe3f5e8db6d33ef1947de81b7b35f2831f8c
Cucu Raul - Mihai <raul.cucu02@e-uvt.ro>
2024-12-10 23:21:34 +0200

    Initial commit

---
+++ src/main/java/org/example/kicksim/model/Player.java
@@ -0,0 +1,14 @@
+package org.example.kicksim.model;
+
+import org.springframework.context.annotation.Bean;
+
+@Bean
+public class Player {
+
+    private String firstName;
+    private String lastName;
+    private Integer age;
+    private Double skillLevel;
+    private Double weight;
+    private Double height;
+}
---
+++ src/test/java/org/example/kicksim/KickSimApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.kicksim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KickSimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
